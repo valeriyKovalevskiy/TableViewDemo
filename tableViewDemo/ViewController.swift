82cbec9097296eb1db782841aeebb8a43f724503
Valeriy Kovalevskiy <vkovalevskiy@wakeapp.ru>
2020-02-10 22:53:08 +0300

    initial commit

---
+++ tableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewDemo
+//
+//  Created by валерий on 2/10/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
